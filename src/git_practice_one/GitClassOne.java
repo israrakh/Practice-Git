f2529fe4ff754cbaf7c3292599263bb880dc260c
Israr <israr.akh03@gmail.com>
2023-05-17 20:44:10 -0400

    I have added my changes

---
+++ src/git_practice_one/GitClassOne.java
@@ -0,0 +1,9 @@
+package git_practice_one;
+
+public class GitClassOne {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Git Class One");
+	}
+}
---
+++ src/git_practice_one/GitClassTwo.java
@@ -0,0 +1,11 @@
+package git_practice_one;
+
+public class GitClassTwo {
+	
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 20; i++) {
+			System.out.println(i);
+		}
+	}
+}
